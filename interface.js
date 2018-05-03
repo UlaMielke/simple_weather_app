6da82b4f5d65c429d14563f8ce359cc1bd86e420
magdalena <magdalena.mielke90@gmail.com>
2018-05-03 10:14:27 +0200

    add interface

---
+++ interface.js
@@ -0,0 +1,15 @@
+class Weather {
+    constructor(city, country, conditions, temp){
+        this.city = city;
+        this.country = country;
+        this.conditions;
+        this.temp = temp;
+
+    }
+}
+
+const CONDITIONS = ["run", "rain", "wind", "snow", "hail", "storm", "cloudy", "partlyCloudy", "fog", "snowAndRain"]
+
+function weatherFor(city) {
+    return Weather("Wrocław", "PL", "snow", 20);
+}
