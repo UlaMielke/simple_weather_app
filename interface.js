34b0d27c8eda7ed0420525a8f5e4f9083659f980
magdalena <magdalena.mielke90@gmail.com>
2018-05-03 12:08:31 +0200

    add basic call to yahoo

--- interface.js
+++ interface.js
@@ -1,8 +1,8 @@
 class Weather {
-    constructor(city, country, conditions, temp){
+    constructor(city, country, conditions, temp) {
         this.city = city;
         this.country = country;
-        this.conditions =conditions;
+        this.conditions = conditions;
         this.temp = temp;
 
     }
@@ -11,5 +11,22 @@ class Weather {
 const CONDITIONS = ["run", "rain", "wind", "snow", "hail", "storm", "cloudy", "partlyCloudy", "fog", "snowAndRain"];
 
 function weatherFor(city) {
-    return Weather("Wrocław", "PL", "snow", 20);
+    let normalizeCity = txtNormalize(city);
+    let url = 'https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + normalizeCity + ', ak")&format=json&env=store://datatables.org/alltableswithkeys';
+    $.getJSON(url)
+        .done(function( yahooWeather ) {
+            console.log( "JSON Data: " + yahooWeather);
+            return Weather("wroclaw","polska","sun", 24);
+
+        })
+        .fail(function( jqxhr, textStatus, error ) {
+            let err = textStatus + ", " + error;
+            console.log( "Request Failed: " + err );
+            return null;
+        });
 }
+
+function txtNormalize(city) {
+    return city.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase()
+}
+
